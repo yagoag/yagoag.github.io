a7719b5b51f985c1bac436a9017278493c26bee8
Yago Arroyo <yagoag@gmail.com>
2020-07-11 19:43:04 -0300

    Add social links

---
+++ src/components/layout.js
@@ -0,0 +1,17 @@
+              <StyledY role="img" aria-label="y" />
+          Built with{' '}
+          <a href="https://www.gatsbyjs.org">
+            <GatsbyIcon
+              width={18}
+              fill="#94a1b2"
+              style={{ marginBottom: -3 }}
+            />
+          </a>
+          Built with{' '}
+          <a href="https://www.gatsbyjs.org">
+            <GatsbyIcon
+              width={18}
+              fill="#94a1b2"
+              style={{ marginBottom: -3 }}
+            />
+          </a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import HiIllustrationDark from '../../content/assets/HiIllustrationDark';
+import GitHubIcon from '../../content/assets/GitHubIcon';
+import LinkedInIcon from '../../content/assets/LinkedInIcon';
+import TwitterIcon from '../../content/assets/TwitterIcon';
 
 type Data = {
   site: {
@@ -30,13 +33,13 @@ type Data = {
 
 const Presentation = styled.div`
   display: flex;
+  text-align: right;
 
-  > p {
+  > div > p {
     font-family: 'Work Sans', -apple-system, 'Open Sans', 'Helvetica Neue',
       sans-serif;
     font-size: 64px;
     font-weight: 600;
-    text-align: right;
     letter-spacing: 0.05rem;
     line-height: 1.25;
     color: #fffffe;
@@ -54,6 +57,20 @@ const Accent = styled.span`
     0 1px 0 #fffffe;
 `;
 
+const SocialLinks = styled.div`
+  > a {
+    transition: opacity 400ms;
+
+    &:not(:last-child) {
+      margin-right: 24px;
+    }
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+`;
+
 const Index = ({ data, location }: PageProps<Data>) => {
   const siteTitle = data.site.siteMetadata.title;
 
@@ -64,11 +81,24 @@ const Index = ({ data, location }: PageProps<Data>) => {
         {/* TODO: switch to light version when theme changes (lights off) */}
         <HiIllustrationDark
           role="img"
-          label="illustration of Yago waving hi behind a laptop, next to a cup of coffee"
+          aria-label="illustration of Yago waving hi behind a laptop, next to a cup of coffee"
         />
-        <p>
-          Hi, I am Yago and I <Accent>love</Accent> front-end development
-        </p>
+        <div>
+          <p>
+            Hi, I am Yago and I <Accent>love</Accent> front-end development
+          </p>
+          <SocialLinks>
+            <a href="https://github.com/yagoag">
+              <GitHubIcon fill="#fffffe" width={24} />
+            </a>
+            <a href="https://www.linkedin.com/in/yagoag/">
+              <LinkedInIcon fill="#fffffe" width={24} />
+            </a>
+            <a href="https://twitter.com/yagoag`">
+              <TwitterIcon fill="#fffffe" width={24} />
+            </a>
+          </SocialLinks>
+        </div>
       </Presentation>
     </Layout>
   );
